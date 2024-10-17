ba2d2f291942384b2c4c46d29b56914bc20f95fe
Tom Dickson <tom@tomdickson.eu>
2024-10-17 17:07:47 +0200

    Small changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,14 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
   // Console log the data to see what's available
   // You can see these logs in the terminal where
   // you run `yarn dev`
+  console.log("HELLO FROM THE IFRAME!");
   console.log({ data });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome to the custom app&nbsp;
+          Welcome to the custom APP YOU FUCKER&nbsp;
+          {JSON.stringify(data)}
           <code className="font-mono font-bold">
             {data.client ? data.client.givenName : data.company?.name}
           </code>
